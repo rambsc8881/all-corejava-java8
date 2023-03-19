71bb534031169fa87a8600feecf85eb5d1cdcb7e
rambsc8881 <rambsc888@gmail.com>
2023-03-19 23:02:15 +0530

    "ram2"

--- src/javabasics/FunctionAndPredicate.java
+++ src/javabasics/FunctionAndPredicate.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
+//
 
     public class FunctionAndPredicate {
 
